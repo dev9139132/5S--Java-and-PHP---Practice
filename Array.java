47025eb12ac7cb83916cbd1f08703df5de223c12
dev9139132 <backupdev4@gmail.com>
2025-06-04 05:21:24 +0000

    practised array

---
+++ Array.java
@@ -0,0 +1,10 @@
+class Array {
+    public static void main(String[] args) {
+        int x[] = new int[5];
+        for (int i = 0; i < x.length; i++) {
+            x[i] = i*2;
+            
+        }
+        System.out.println(x[2]);
+    }
+}
--- Hello.java
+++ Hello.java
@@ -1,7 +1,25 @@
+class NewClass
+{
+    static void func1()
+    {
+        System.out.println("Output from funcfunc1");
+    }
+
+    static void func2()
+    {
+        System.out.println("Output from func2");
+    }
+}
+
+
 class Hello
 {
     public static void main(String []args)
     {
-        System.out.println("Hello, world");
+        NewClass n1 = new NewClass();
+        n1.func1();
+        n1.func2();
+
     }
 }
+funcfunc
