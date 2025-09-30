fdc3088fd7420dfb08d539269dd948f7733671e6
dev9139132 <backupdev4@gmail.com>
2025-09-30 14:44:18 +0530

    Update 4.php

--- php-dev-environment/4.php
+++ php-dev-environment/4.php
@@ -1,24 +1,19 @@
 <?php
 
-function isIsoceles($a,$b,$c)
+function isIsosceles($a, $b, $c)
 {
-    if($a<$b+$c && $b<$a+$c && $c<$a+$b)
-    {
-        if($a==$b || $b == $c || $a  == $c)
-        {
-            echo "Triangle with sides $a,$b and $c is an isoleces triangle\n";
+    if ($a < $b + $c && $b < $a + $c && $c < $a + $b) {
+        if ($a == $b || $b == $c || $a == $c) {
+            echo "Triangle with sides $a, $b and $c is an isosceles triangle\n";
+        } else {
+            echo "Triangle with sides $a, $b and $c is not an isosceles triangle\n";
         }
-        else
-        {
-              echo "Triangle with sides $a,$b and $c is not an isoleces triangle\n";
-
-        }
+    } else {
+        echo "Not a valid triangle\n";
     }
-    else echo "Not a valid triangle\n";
 }
 
-isIsoceles(1,2,2);
-isIsoceles(1,3,2);
-
+isIsosceles(1, 2, 2);
+isIsosceles(1, 3, 2);
 
 ?>
