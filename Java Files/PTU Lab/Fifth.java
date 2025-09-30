8432722b1d6f2e70b1c7e33395dcedc07b1f5777
dev9139132 <backupdev4@gmail.com>
2025-09-30 21:06:05 +0530

    Update Fifth.java

--- Java Files/PTU Lab/Fifth.java
+++ Java Files/PTU Lab/Fifth.java
@@ -2,9 +2,9 @@
 
 public class Fifth {
     public static void main(String[] args) {
-        final double ONE_INCH = 39.3700787402; // meters
+        final double ONE_METER = 39.3700787402; // inches
         double inches = 50.0;
-        double meters = inches/ONE_INCH;
+        double meters = inches/ONE_METER;
         System.out.printf("%.2f inches in meters are: %.2f%n", inches,meters);
     }   
 }
